7df79609a4b4e5f37147249c3affaeb4760f77c7
Devin Withoft <teddyfest@gmail.com>
2022-12-11 22:55:27 -0700

    Requirements met, I hope

--- src/AppState.js
+++ src/AppState.js
@@ -5,15 +5,16 @@ export const AppState = reactive({
   user: {},
   /** @type {import('./models/Account.js').Account} */
   account: {},
+  /** @type {import('./models/Account.js').Account} */
+  activeProfile: {},
   /** @type {import('./models/Post.js').Post[]} */
   posts: [],
   page: 1,
-  maxPage: null,
-  /** @type {import('./models/Account.js').Account} */
-  activeProfile: null,
+  maxPage: 0,
   page: [],
-
+  /** @type {import('./models/Post.js').Post} */
+  activePost: {},
   query: '',
   /** @type {import('./models/Product.js').Product[]} */
-  products: []
+  products: [],
 })
